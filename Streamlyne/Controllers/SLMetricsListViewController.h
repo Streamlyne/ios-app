4e296c5a0ca68f8f65d7765265f732c966cfb953
Glavin Wiechert <glavin@streamlyne.co>
2014-07-22 14:26:34 -0300

    See #7. Work on Metrics route UI.

---
+++ Streamlyne/Controllers/SLMetricsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLMetricsListViewController.h
+//  Streamlyne
+//
+//  Created by Glavin Wiechert on 2014-07-22.
+//  Copyright (c) 2014 Streamlyne Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLMetricsListViewController : UITableViewController
+
+@end
